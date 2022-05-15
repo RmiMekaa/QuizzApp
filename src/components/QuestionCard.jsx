bed4df446e2df2529b429de2a2cfa0e1011ec0c8
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-15 21:28:05 +0200

    Config QuestionCard props

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
 /**
- * Question card component
+ * Question card component 
  * @component 
  */
-export default function QuestionCard() {
+export default function QuestionCard({ i, question, answers }) {
   return (
     <article className='questionCard'>
       <div className='questionCard__header'>
-        <div className='questionCard__header__counter'>01</div>
-        <span className='questionCard__header__question'>À quel peintre doit-on « Le Voyageur contemplant une mer de nuages » ? </span>
+        <div className='questionCard__header__counter'>{i}</div>
+        <span className='questionCard__header__question'>{question} </span>
       </div>
       <div className='questionCard__timer'></div>
       <div className='questionCard__answers'>
-        <button>Gustave Courbet</button>
-        <button>Caspar David Friedrich</button>
-        <button>Théodore Géricault</button>
-        <button>Eugène Delacroix</button>
+        {answers.map(answer => <button>{answer}</button>)}
       </div>
     </article>
   )
+}
+
+QuestionCard.propTypes = {
+  i: PropTypes.number.isRequired,
+  question: PropTypes.string.isRequired,
+  answers: PropTypes.array.isRequired,
 }
--- src/pages/QuizzPage.jsx
+++ src/pages/QuizzPage.jsx
@@ -4,7 +4,7 @@ import QuestionCard from '../components/QuestionCard'
 export default function quizzPage() {
   return (
     <main className='quizzPage'>
-      < QuestionCard />
+      < QuestionCard i={1} question={"Bla bla bla bla bla ?"} answers={['1', '2', '3', '4']} />
     </main>
   )
 }
