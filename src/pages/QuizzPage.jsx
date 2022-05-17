594ecaf57f32bd0955ed116fc843a84ff9d00bef
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-17 20:37:21 +0200

    Create Results component

--- src/pages/QuizzPage.jsx
+++ src/pages/QuizzPage.jsx
@@ -1,28 +1,18 @@
 import React, { useState } from 'react'
 import QuestionCard from '../components/QuestionCard'
 import questions from '../mockData/questions.js'
-
+import Results from '../components/Results'
 
 export default function QuizzPage() {
   const [questionNumber, setquestionNumber] = useState(0)
   const [userAnswers, setUserAnswers] = useState([]);
-
-  const currentQuestion = questions[questionNumber]
-  let i = 0;
+  const quizz = questions;
 
   return (
     <main className='quizzPage'>
-      {(questionNumber > questions.length - 1)
-        ? (
-          <div style={{ color: 'white' }}>
-            <h1 >Results : </h1>
-            {userAnswers.map(answer => {
-              i++;
-              return <span>Question{i} : {answer}</span>
-            })}
-          </div>
-        )
-        : <QuestionCard question={currentQuestion} {...{ questionNumber, setquestionNumber, userAnswers, setUserAnswers }} />
+      {(questionNumber > quizz.length - 1)
+        ? <Results {...{ quizz, userAnswers }} />
+        : <QuestionCard question={quizz[questionNumber]} {...{ questionNumber, setquestionNumber, userAnswers, setUserAnswers }} />
       }
     </main>
   )
