257d66a980dd8e83ed06a76cac57932b6a4efada
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-17 15:54:22 +0200

    Add state, props passing and hard data to test

--- src/pages/QuizzPage.jsx
+++ src/pages/QuizzPage.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import QuestionCard from '../components/QuestionCard'
 
-export default function quizzPage() {
+const questions = [
+  {
+    title: 'À quel peintre doit-on « Le Voyageur contemplant une mer de nuages » ?',
+    answers: ['Gustave Courbet', 'Caspar David Friedrich', 'Théodore Géricault', 'Eugène Delacroix'],
+    correct_answer: 'Caspar David Friedrich'
+  },
+  {
+    title: 'Qui a peint « Le baiser » ?',
+    answers: ['Ferdinand Hodler', 'Odilon Redon', 'Gustave Klimt', 'Alphonse Osbert'],
+    correct_answer: 'Gustave Klimt'
+  }
+]
+
+export default function QuizzPage() {
+  const [questionNumber, setquestionNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const currentQuestion = questions[questionNumber]
+  let i = 0;
+
   return (
     <main className='quizzPage'>
-      < QuestionCard i={1} question={"Bla bla bla bla bla ?"} answers={['1', '2', '3', '4']} />
+      {(questionNumber > questions.length - 1)
+        ? (
+          <div style={{ color: 'white' }}>
+            <h1 >Results : </h1>
+            {userAnswers.map(answer => {
+              i++;
+              return <span>Question{i} : {answer}</span>
+            })}
+          </div>
+        )
+        : <QuestionCard question={currentQuestion} {...{ questionNumber, setquestionNumber, userAnswers, setUserAnswers }} />
+      }
     </main>
   )
 }
