1fe21b2e8fca84a06259b73c0c89ae5fba16d42c
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-17 15:52:45 +0200

    Add timer and animations

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from "prop-types"
 
 /**
  * Question card component 
  * @component 
  */
-export default function QuestionCard({ i, question, answers }) {
+export default function QuestionCard({ question, questionNumber, setquestionNumber, userAnswers, setUserAnswers }) {
+  const [swipeCard, toggleSwipeCard] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => handleClick(), 20000); //Set timer to auto submit after 20s
+    return () => clearTimeout(timer) //Clear timer when the component will unmount
+  }, [questionNumber, setquestionNumber, userAnswers, setUserAnswers, handleClick])
+
   return (
-    <article className='questionCard'>
+    <article className={swipeCard ? 'questionCard swipe' : 'questionCard'}>
       <div className='questionCard__header'>
-        <div className='questionCard__header__counter'>{i}</div>
-        <span className='questionCard__header__question'>{question} </span>
+        <div className='questionCard__header__counter'>{questionNumber + 1}</div>
+        <span className='questionCard__header__question'>{question.title}</span>
       </div>
-      <div className='questionCard__timer'></div>
+      <div key={'question ' + questionNumber + ' timer'} className='questionCard__timer'></div> {/*Add a key to force rerender of timer*/}
       <div className='questionCard__answers'>
-        {answers.map(answer => <button>{answer}</button>)}
+        {question.answers.map(answer => {
+          let key = 'answer_' + question.answers.indexOf(answer);
+          return (
+            <button key={key} onClick={() => handleClick(answer)}>
+              {answer}
+            </button>
+          )
+        })}
       </div>
-    </article>
+    </article >
   )
+
+  function handleClick(answer) {
+    toggleSwipeCard(true)
+    setUserAnswers([...userAnswers, answer])
+    setTimeout(() => {
+      setquestionNumber(questionNumber + 1)
+      toggleSwipeCard(false)
+    }, 800)
+  }
+
 }
 
 QuestionCard.propTypes = {
-  i: PropTypes.number.isRequired,
-  question: PropTypes.string.isRequired,
-  answers: PropTypes.array.isRequired,
+  question: PropTypes.object.isRequired,
 }
