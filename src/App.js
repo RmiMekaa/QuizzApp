2e05522f1cdb69e3267fb470d4eb83468499ddbd
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-18 17:48:48 +0200

    Move state on the top level of the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Header from "./components/Header.jsx";
 import QuizzPage from "./pages/QuizzPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 
 export default function App() {
+  const [difficulty, setDifficulty] = useState('Any');
+  const [category, setCategory] = useState('Any');
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/quizz" element={<QuizzPage />} />
+        <Route path="/" element={<HomePage {...{ difficulty, category, setDifficulty, setCategory }} />} />
+        <Route path="/quizz" element={<QuizzPage {...{ difficulty, category }} />} />
       </Routes>
     </BrowserRouter>
   );
