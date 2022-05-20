ab80da68edf38a6eeda336bd95b942482e190f0b
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-20 13:33:50 +0200

    Remove results route as results is now a simple component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import { useState } from "react";
 import Header from "./components/Header.jsx";
 import QuizzPage from "./pages/QuizzPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import ResultsPage from "./pages/ResultsPage.jsx";
 
 export default function App() {
   const [difficulty, setDifficulty] = useState('Any');
   const [category, setCategory] = useState('Any');
-  const [quizz, setQuizz] = useState(null)
 
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage {...{ difficulty, category, setDifficulty, setCategory }} />} />
-        <Route path="/quizz" element={<QuizzPage {...{ difficulty, category, quizz, setQuizz }} />} />
-        <Route path="/results" element={<ResultsPage {...{ quizz }} />} />
+        <Route path="/quizz" element={<QuizzPage {...{ difficulty, category }} />} />
       </Routes>
     </BrowserRouter>
   );
