9c7930822470bd6aa044b9c710112d6e81102d19
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-23 14:34:43 +0200

    Add select buttons options data

---
+++ src/data/SelectOptions.js
@@ -0,0 +1,24 @@
+const categoryOptions = [
+  "Any",
+  "Arts & Literature",
+  "Film & TV",
+  "Food & Drink",
+  "General Knowledge",
+  "Geography",
+  "History",
+  "Music",
+  "Science",
+  "Society & Culture",
+  "Sport & Leisure"
+]
+
+const difficultyOptions = [
+  "Any",
+  "Easy",
+  "Medium",
+  "Hard"
+]
+
+const amountOptions = [1, 5, 10, 20]
+
+export { categoryOptions, difficultyOptions, amountOptions }
