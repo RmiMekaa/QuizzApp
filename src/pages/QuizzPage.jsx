786b974741a4b7c06ddeaa3d85cd70501123b714
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-23 14:41:55 +0200

    Add quantity prop

--- src/pages/QuizzPage.jsx
+++ src/pages/QuizzPage.jsx
@@ -3,13 +3,13 @@ import QuestionCard from '../components/QuestionCard'
 import useGetQuizz from '../hooks/useGetQuizz'
 import Results from '../components/Results'
 
-export default function QuizzPage({ category, difficulty }) {
+export default function QuizzPage({ category, difficulty, quantity }) {
   const [index, updateIndex] = useState(0)
-  const { loading, quizz, error } = useGetQuizz(difficulty, category)
+  const { loading, quizz, error } = useGetQuizz(category, difficulty, quantity)
 
   if (!quizz) return;
   if (loading) return <span>Loading...</span>
-  if (error) console.error(error)
+  if (error) console.log(error)
 
   return (
     <main className='quizzPage'>
