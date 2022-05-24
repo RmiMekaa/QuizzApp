94aeb5ee98dff593ede9d48bb1c84f1f7f5a7e5a
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-24 14:54:22 +0200

    Refcato: Minor changes

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,18 +4,18 @@ import { categories } from '../data/categories'
 export default function Categories({ selectedCategory, setCategory }) {
   return (
     <section className='categories'>
-      <h3 className='categories__heading'>Categories</h3>
+      <h3 className='categories__heading'>Category</h3>
       <div className='categories__container'>
         {categories.map(category => {
           const background = require('../images/categories/' + category.image);
 
           return (
-            <article
+            <button
               className={(category.name === selectedCategory) ? 'categoryCard categoryCard--active' : 'categoryCard'}
               onClick={() => setCategory(category.name)}>
               <img src={background} alt=""></img>
               <h4>{category.name}</h4>
-            </article>
+            </button>
           )
         })}
       </div>
