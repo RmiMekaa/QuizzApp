f1212d0fed828f2afcdfa2e6692cf0b1a37097bc
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-24 13:32:21 +0200

    refactor: Merge components

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { categories } from '../data/categories'
-import CategoryCard from './CategoryCard'
 
 export default function Categories({ selectedCategory, setCategory }) {
   return (
     <section className='categories'>
       <h3 className='categories__heading'>Categories</h3>
       <div className='categories__container'>
-        {categories.map(category => <CategoryCard {...{ category, selectedCategory, setCategory }} />)}
+        {categories.map(category => {
+          const background = require('../images/categories/' + category.image);
+
+          return (
+            <article
+              className={(category.name === selectedCategory) ? 'categoryCard categoryCard--active' : 'categoryCard'}
+              onClick={() => setCategory(category.name)}>
+              <img src={background} alt=""></img>
+              <h4>{category.name}</h4>
+            </article>
+          )
+        })}
       </div>
     </section>
   )
