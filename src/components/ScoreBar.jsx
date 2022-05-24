6491b06284ba7bea99d30485303d786d8571531d
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-24 12:37:36 +0200

    Add loader and custom text to scoreBar

--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -6,12 +6,26 @@ export default function ScoreBar({ quizz, score }) {
     width: `${percentage}%`,
   }
 
+  const text = () => {
+    if (score === quizz.length) return "Bravo, c'est un perfect !"
+    if (score > quizz.length / 2) return "Pas mal pour un amateur..."
+    if (score === quizz.length / 2) return 'Ouf ! Tout juste la moyenne !'
+    if (score < quizz.length / 2) return "Oups, des révisions s'imposent !"
+    if (score === 0) return "Aïe..."
+  }
+
   return (
-    <div className={'scoreBar'}>
-      <div className={'scoreBar__label'}><span>{score}/{quizz.length}</span></div>
-      <div className={'scoreBar__container'}>
-        <div className={'scoreBar__container__filler'} style={fillerWidth}></div>
+    <div className='scoreBar'>
+      <div className='scoreBar__label'>
+        <span>{score}/{quizz.length}</span>
+        <svg className="scoreBar__spinner" viewBox="0 0 50 50">
+          <circle cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+        </svg>
       </div>
+      <div className='scoreBar__container'>
+        <div className='scoreBar__container__filler' style={fillerWidth}></div>
+      </div>
+      <span className='scoreBar__text'>{text()}</span>
     </div>
   )
 }
