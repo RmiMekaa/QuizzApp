5c4750ebe1e1364a83a7eac5038b0e38b318f1cf
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-27 15:50:04 +0200

    feat: Implement form to create your own quizzes - WIP

---
+++ src/pages/CreateQuizPage.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import shuffle from '../utils/shuffle'
+
+const initialState = {
+  newQuiz: { name: '', quiz: [] },
+  newQuestion: {
+    question: '',
+    correctAnswer: '',
+    wrongAnswer1: '',
+    wrongAnswer2: '',
+    wrongAnswer3: ''
+  }
+}
+
+export default function CreateQuizPage({ appState, setAppState }) {
+  const [newQuiz, setnewQuiz] = useState(initialState.newQuiz);
+  const [newQuestion, setNewQuestion] = useState(initialState.newQuestion);
+  const [displayModale, toggleModale] = useState(false)
+
+  console.log(newQuiz);
+
+  return (
+    <div className='createQuizPage'>
+      {displayModale ? <span onClick={() => { toggleModale(v => !v); setnewQuiz(initialState.newQuiz) }}>Quiz created !</span> : (
+
+        <form className='form'>
+          <h2>New Quiz</h2>
+
+          <div className='form__quiz'>
+            <input
+              id='quizName'
+              type='text'
+              placeholder='Quiz name...'
+              minLength={3}
+              maxLength={25}
+              value={newQuiz.name}
+              onChange={(e) => setnewQuiz({ ...newQuiz, name: e.target.value })}
+            ></input>
+            <span>Number of questions : {newQuiz.quiz.length}</span>
+            <aside id='modale'>Question created !</aside>
+          </div>
+
+          <div className='form__newQuestion'>
+            <h3>Add question</h3>
+            <input id='question' placeholder='Your question here...' type='text' minLength={10} maxLength={100} value={newQuestion.question} onChange={(e) => handleChange(e)}></input>
+            <div className='form__answers'>
+              <input id='correctAnswer' placeholder='Correct answer...' name='correctAnswer' type='text' maxLength={50} value={newQuestion.correctAnswer} onChange={(e) => handleChange(e)} />
+              <input id='wrongAnswer1' placeholder='Wrong answer 1...' type='text' maxLength={50} value={newQuestion.wrongAnswer1} onChange={(e) => handleChange(e)} />
+              <input id='wrongAnswer2' placeholder='Wrong answer 2...' type='text' maxLength={50} value={newQuestion.wrongAnswer2} onChange={(e) => handleChange(e)} />
+              <input id='wrongAnswer3' placeholder='Wrong answer 3...' type='text' maxLength={50} value={newQuestion.wrongAnswer3} onChange={(e) => handleChange(e)} />
+            </div>
+            <button className='addQuestion' onClick={(e) => addQuestion(e)}>Add question</button>
+          </div>
+
+          <button className='createQuiz' type='submit' onClick={() => createQuiz()}>Create quiz</button>
+        </form>
+      )}
+    </div >
+  )
+
+  /**
+   * Update the corresponding state value on change
+   * @param {*} e Event
+   * @return {void}
+   */
+  function handleChange(e) {
+    const { id, value } = e.target;
+    setNewQuestion((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }))
+  }
+
+  function addQuestion(e) {
+    e.preventDefault();
+    let formatQuestion = {
+      answers: shuffle([newQuestion.correctAnswer, newQuestion.wrongAnswer1, newQuestion.wrongAnswer2, newQuestion.wrongAnswer3]),
+      correctAnswer: newQuestion.correctAnswer,
+      question: newQuestion.question,
+    }
+    setnewQuiz({ ...newQuiz, quiz: [...newQuiz.quiz, formatQuestion] });
+    setNewQuestion(initialState.newQuestion)
+    // Trigger animation ↓
+    const modale = document.getElementById('modale');
+    modale.classList.add('display');
+    setTimeout(() => {
+      modale.classList.remove('display')
+    }, 2000)
+  }
+
+  function createQuiz() {
+    setAppState({ ...appState, customQuizzes: [...appState.customQuizzes, { name: newQuiz.name, quiz: newQuiz.quiz }] })
+    toggleModale(v => !v)
+  }
+}
