356b3a8754510d198d0957c709bb05adfd59855e
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-27 15:48:35 +0200

    Create global state for the app and make it persistent

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header.jsx";
 import Footer from './components/Footer.jsx';
 import QuizPage from "./pages/QuizPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import CreateQuizPage from "./pages/CreateQuizPage.jsx";
 
+const initialState = {
+  selectedCategory: 'Any',
+  selectedDifficulty: 'Any',
+  selectedQuantity: 10,
+  customQuizzes: []
+}
+
 export default function App() {
-  const [selectedDifficulty, setDifficulty] = useState('Any');
-  const [selectedCategory, setCategory] = useState('Any');
-  const [selectedQuantity, setQuantity] = useState(10);
-  const [customQuizes, setCustomQuizes] = useState([])
+  const [appState, setAppState] = useState(initialState);
+
+  useEffect(() => {
+    let retrievedState = JSON.parse(window.localStorage.getItem("appState"));
+    retrievedState && setAppState(retrievedState);
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("appState", JSON.stringify(appState));
+  }, [appState]);
 
   return (
     <BrowserRouter>
       <Header />
       <main>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <HomePage {...{
-                selectedDifficulty,
-                setDifficulty,
-                selectedCategory,
-                setCategory,
-                selectedQuantity,
-                setQuantity
-              }}
-              />}
-          />
-          <Route path="/quiz" element={<QuizPage {...{ selectedCategory, selectedDifficulty, selectedQuantity }} />} />
-          <Route path="/create-quiz" element={<CreateQuizPage />} />
+          <Route path="/" element={<HomePage {...{ appState, setAppState }} />} />
+          <Route path="/quiz" element={<QuizPage {...{ appState }} />} />
+          <Route path="/create-quiz" element={<CreateQuizPage {...{ appState, setAppState }} />} />
         </Routes>
       </main>
       <Footer />
