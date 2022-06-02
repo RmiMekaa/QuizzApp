2fdafa6f020b1a1555a192ede6bebad54142a83f
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-06-02 15:37:20 +0200

    Don't fetch anything if the user has selected a custom quiz

--- src/hooks/useGetQuiz.js
+++ src/hooks/useGetQuiz.js
@@ -2,42 +2,40 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import formatQuiz from '../utils/formatQuiz'
 
-/**
- * Custom Hook to fetch a quiz from The Trivia API
- * @param   {String}  category    The required category
- * @param   {String}  difficulty  The required difficulty
- * @param   {Number}  quantity    The required number of questions
- * @return  {Object}  The state for loading, quiz and error
- */
-export default function useGetQuiz(category, difficulty, quantity) {
+export default function useGetQuiz(appState) {
+  const { selectedQuiz, selectedCategory, selectedDifficulty, selectedQuantity } = appState
   const [quiz, setQuiz] = useState(null)
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
 
   const baseUrl = "https://the-trivia-api.com/api/questions?";
 
+  function configUrl(category, difficulty, quantity) {
+    let difficultyParam = '';
+    let categoryParam = '';
+    let quantityParam = '&limit=' + quantity;
+    if (category !== 'Any') categoryParam = "&categories=" + category.toLowerCase().replaceAll('&', 'and').replaceAll(' ', '_')
+    if (difficulty !== 'Any') difficultyParam = "&difficulty=" + difficulty.toLowerCase();
+    return baseUrl + categoryParam + quantityParam + difficultyParam
+  }
+
   useEffect(() => {
     setLoading(true)
-    const url = () => {
-      let difficultyParam = '';
-      let categoryParam = '';
-      let quantityParam = '&limit=' + quantity;
-      if (category !== 'Any') categoryParam = "&categories=" + category.toLowerCase().replaceAll('&', 'and').replaceAll(' ', '_')
-      if (difficulty !== 'Any') difficultyParam = "&difficulty=" + difficulty.toLowerCase();
-      return baseUrl + categoryParam + quantityParam + difficultyParam
+    if (selectedQuiz) setQuiz(selectedQuiz);
+    else {
+      let url = configUrl(selectedCategory, selectedDifficulty, selectedQuantity)
+      axios.get(url)
+        .then(response => {
+          let quiz = formatQuiz(response.data) //Format quiz
+          setQuiz(quiz)
+        })
+        .catch(err => setError(err))
+        .finally(() => setLoading(false))
     }
+    setLoading(false)
 
-    axios
-      .get(url())
-      .then(response => {
-        console.log(response.data);
-        let quiz = formatQuiz(response.data) //Format quiz
-        setQuiz(quiz)
-      })
-      .catch(err => setError(err))
-      .finally(() => setLoading(false))
-  }, [category, difficulty, quantity])
+  }, [selectedQuiz, selectedCategory, selectedDifficulty, selectedQuantity])
 
-  return { loading, quiz, error }
+  return { loading, error, quiz }
 }
 
