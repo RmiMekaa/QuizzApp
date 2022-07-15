03cc74e8967c92324507552fee379fd7060ba8fc
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-07-15 16:49:53 +0200

    Add emailto link so users can contact me

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-      <span>
-        © Mekaa · 2022 · Questions provided by <a href='https://the-trivia-api.com/' target="_blank" rel="noreferrer">The Trivia API</a>
-      </span>
+      <span>© Mekaa · 2022 · Questions provided by <a href='https://the-trivia-api.com/' target="_blank" rel="noreferrer">The Trivia API</a></span>
+      <span>Find a bug ? Have a suggestion ? Please let me know: <a href="mailto:roeland.remi@gmail.com">roeland.remi@gmail.com</a></span>
     </footer>
   )
 }
