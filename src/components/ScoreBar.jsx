ce3338cc892e63ba2e2c6110140b72704d3a771a
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-07-20 08:47:36 +0200

    Responsive improvments

--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -10,22 +10,22 @@ export default function ScoreBar({ quiz, score }) {
     if (percentage === 100) return "Perfect score, congratulations !"
     if (percentage > 75 && percentage < 100) return "Great score ! But is this the best you can do ?"
     if (percentage >= 50 && percentage < 75) return "Not bad, but you could do better !"
-    if (percentage < 50 && percentage > 0) return "Oops...I won't pretend that it is a good result, sorry"
+    if (percentage < 50 && percentage > 0) return "I was programmed to be honest : this is pretty bad, sorry"
     if (percentage === 0) return "Ouch...From zero to hero ?"
   }
 
   return (
-    <div className='scoreBar'>
-      <div className='scoreBar__label'>
-        <span>{score}/{quiz.length}</span>
-        <svg className="scoreBar__spinner" viewBox="0 0 50 50">
-          <circle cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
-        </svg>
+    <>
+      <div className='scoreBar'>
+        <div className='scoreBar__label'>
+          <span>{score}/{quiz.length}</span>
+          <div className="scoreBar__spinner"></div>
+        </div>
+        <div className='scoreBar__container'>
+          <div className='scoreBar__container__filler' style={fillerWidth}></div>
+        </div>
+        <span className='scoreBar__text'>{text()}</span>
       </div>
-      <div className='scoreBar__container'>
-        <div className='scoreBar__container__filler' style={fillerWidth}></div>
-      </div>
-      <span className='scoreBar__text'>{text()}</span>
-    </div>
+    </>
   )
 }
